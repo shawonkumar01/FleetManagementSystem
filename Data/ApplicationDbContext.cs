67b8c398266aa97f6c223a0f0eecf8568668ebaa
Shawon Kumar Modak <>
2026-05-03 21:07:05 +0600

    Initial commit Project setup

---
+++ Data/ApplicationDbContext.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace FleetManagementSystem.Data
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+            : base(options)
+        {
+        }
+
+        // DbSets will go here later (Vehicles, Drivers, etc.)
+    }
+}
